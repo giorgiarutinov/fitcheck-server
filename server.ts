0dfa8ba08c8afa9cecadaa3ea31ee7fad4e8a5fe
giorgiarutinov <98847943+giorgiarutinov@users.noreply.github.com>
2025-04-13 09:23:13 +0400

    server.ts

--- server.ts
+++ server.ts
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
 app.use(cors());
 app.use(express.json());
 
-app.post('/analyze-style', async (req: Request, res: Response) => {
+app.post('/analyze-style', async (req: Request<{}, {}, { photoUrl: string }>, res: Response) => {
   console.log('\nProcessing analyze request...');
   
   try {
